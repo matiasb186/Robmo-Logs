226a89188d9ba29b66d4f71ad273aa7bb52659c4
matiasb186 <86837307+matiasb186@users.noreply.github.com>
2023-11-16 17:34:14 -0500

    Add files via upload

---
+++ Resources/Packages.bat
@@ -0,0 +1,32 @@
+@echo off
+
+title R0BM0 STEALER
+
+python -m pip install -U pip
+
+pip install requests
+pip install pycryptodome
+pip install pystyle
+pip install colorama
+pip install auto_py_to_exe
+pip install pyinstaller
+pip install wmi
+pip install psutil
+pip install cryptography
+pip install anonfile
+pip install pypiwin32
+pip install regex
+pip install addict
+pip install requests-toolbelt
+pip install FirefoxDecrypt
+pip install cx-Freeze
+pip install pyuac
+pip install customtkinter
+pip install pyautogui
+pip install telegram
+pip install tkintertable
+pip install tk
+pip install python-telegram-bot
+pip install pycryptodome
+pip install Pillow
+pip install pybase64
